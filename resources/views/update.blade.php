f42e13b67b0f971ba79ed940fb8e91b7bf641c87
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-15 22:41:06 +0000

    update page to be display dynamically with the unique post id

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -17,25 +17,30 @@
             <div class="card-body">
                 {{-- This is a form page --}}
                 <form action="">
+                    @csrf
+                    <div>
+                        <img src="{{asset('storage/' . $post->image)}}" alt="" width="200px">
+                    </div>
                     <div class="form-group mt-2">
                         <label for="image">Image</label>
                         <input type="file" name="image" class="form-control">
                     </div>
                     <div class="form-group mt-2">
                         <label for="title">Title</label>
-                        <input type="name" name="title" class="form-control">
+                        <input type="name" name="title" value="{{$post->title}}" class="form-control">
                     </div>
                     <div class="form-group mt-2">
                         <label for="title">Category</label>
                         <select name="" id="" class="form-control">
-                            <option value="">Test 1</option>
-                            <option value="">Test 2</option>
-                            <option value="">Test 3</option>
+                            <option value="">Select Category</option>
+                            @foreach ($categories as $category)
+                                <option {{$category->id == $post->category_id ? 'selected' : ''}} value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group mt-2">
                         <label for="descripion">Description</label>
-                        <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
+                        <textarea name="description" id="" cols="30" rows="10" class="form-control">{{$post->description}}</textarea>
                     </div>
                     <div class="form-group mt-2">
                         <button type="submit" class="btn btn-primary">Submit</button>
