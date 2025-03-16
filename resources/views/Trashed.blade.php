7a341b10b65437c6143d458cce1f3a1110788913
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-16 22:54:30 +0000

    display trashed posts

--- resources/views/Trashed.blade.php
+++ resources/views/Trashed.blade.php
@@ -6,7 +6,7 @@
         <div class="card-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h3><a href="{{route('posts.index')}}" style="text-decoration: none;">Trashed Posts</a></h3>
+                    <h3>Trashed Blog Posts</h3>
                 </div>
                 <div class="col-md-6 d-flex justify-content-end">
                     <a href="{{route('posts.index')}}" class="btn btn-dark mx-1"><i class="fa-solid fa-arrow-left"></i> Back</a>
@@ -28,21 +28,26 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>
-                            <img src="https://picsum.photos/200" alt="" width="80">
-                        </td>
-                        <td>Post Title</td>
-                        <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut autem quod mollitia incidunt eius dolorem quos modi quas itaque ex!</td>
-                        <td>Crud</td>
-                        <td>28-02-2025</td>
-                        <td>
-                            <a href="" class="btn-sm btn-primary" style="text-decoration: none;">Show</a>
-                            <a href="" class="btn-sm btn-warning" style="text-decoration: none;">Edit</a>
-                            <a href="" class="btn-sm btn-danger" style="text-decoration: none;">Delete</a>
-                        </td>
-                    </tr>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th scope="row">{{$post->id}}</th>
+                            <td>
+                                <img src="{{asset('storage/' . $post->image)}}" alt="" width="80">
+                            </td>
+                            <td>{{$post->title}}</td>
+                            <td>{{$post->description}}</td>
+                            <td>{{$post->category->name}}</td>
+                            <td>{{date('d-M-Y', strtotime($post->created_at))}}</td>
+                            <td>
+                                <a href="{{route('posts.restore', $post->id)}}" class="btn-sm btn-primary" style="text-decoration: none;">Restore</a>
+                                <form action="{{route('posts.force-delete', $post->id)}}" method="POST" style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn-sm btn-danger mt-1" style="text-decoration: none;">Delete Permanetly</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
